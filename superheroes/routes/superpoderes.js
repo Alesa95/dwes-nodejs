80d170b5e59d223d13f2fa26eae7b535940ca2cb
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-02-07 10:54:00 +0100

    conexion con mongodb

--- superheroes/routes/superpoderes.js
+++ superheroes/routes/superpoderes.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+const database = client.db('superheroes');
+const superpoderesCollection = database.collection("superpoderes");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var arraySuperpoderes = [
+router.get('/', async(req, res, next) => {
+    /*var arraySuperpoderes = [
         {  
             superheroe: "Superman", 
             superpoder: "Volar"
@@ -18,14 +24,27 @@ router.get('/', function(req, res, next) {
         },
         {
             superheroe: "Viuda Negra", 
-            supderpoder: "Superagilidad"
+            superpoder: "Superagilidad"
         }
-    ];
+    ];*/
+
+    var arraySuperpoderes = getAllSuperpoderes();
 
     res.render('superpoderes/index', {
         title: 'Superpoderes', 
-        superpoderes: arraySuperpoderes 
+        superpoderes: await arraySuperpoderes 
     });
 });
+
+async function getAllSuperpoderes() {
+    try {
+        var query = {};
+        var superpoderes = await superpoderesCollection.find(query).toArray();
+        //console.log(superpoderes.length);
+        return superpoderes;
+    } finally {
+        // esto es una guarrada, deberíamos hacer un catch
+    }
+}
 
 module.exports = router;
